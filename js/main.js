05c22e0171c1aa99932c6e1731755d9ccad276e2
loooped <lovemarinko@ukr.net>
2015-11-16 13:50:39 +0200

    First commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+﻿window.onload = function () {
+    var firstHoverItem = document.getElementById("first-hover-item");
+    var productsSubmenu = document.getElementById("produkty-submenu");
+    firstHoverItem.onmouseover = function () {
+        productsSubmenu.style.display = "block";
+        productsSubmenu.onmousemove = function () {
+            productsSubmenu.style.display = "block";
+        }
+        productsSubmenu.onmouseover = function () {
+            productsSubmenu.style.display = "block";
+        }
+        productsSubmenu.onmouseout = function () {
+            productsSubmenu.style.display = "none";
+        }
+    }
+    firstHoverItem.onmouseout = function () {
+        productsSubmenu.style.display = "none";
+    }
+
+}
